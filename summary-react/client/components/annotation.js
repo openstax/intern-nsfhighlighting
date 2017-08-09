71d8b31394d10dbde696567241b67943d20bc191
danya3781 <da30@rice.edu>
2017-08-09 10:34:15 -0500

    Filtering by annotation vs highlight, general modularity, includes session data, and settings page.

--- summary-react/client/components/annotation.js
+++ summary-react/client/components/annotation.js
@@ -2,26 +2,78 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var axios = require('axios');
 var Radium = require('radium');
-
+var _ = require('lodash');
+import ReactLoading from 'react-loading';
 
 var styles = {
   annotation: {
-    width:"400px",
+    width:"470px",
     textAlign:"left",
   },
   text:{
-    color: "#222b4f"
+    color: "#222b4f",
+    width:'400px',
+    overflow:"hidden"
   },
   target:{
     color: "#e36f4a"
   },
   list: {
     listStyle:"none"
+  },
+  annotationBorder: {
+    border: '2px solid #585a58',
+    borderRadius: '5px',
+    paddingLeft:'15px',
+    paddingRight:'15px',
+    overflow:"hidden"
+
   }
+
 };
 
+var AnnotationBox = React.createClass({
+
+
+  render: function(){
+    var i = this.props.i
+    var title = this.props.title
+    if (this.props.annotation == true){
+      styles.annotationBorder.border = '2px solid #585a58'
+    }else{
+      styles.annotationBorder.border = '2px solid #e36f4a'
+      title = 'Highlight'
+    }
+
+
+
+    return(
+      <div>
+        <div style={styles.annotationBorder}>
+          <div>
+          <li key={"li-"+i}>
+
+            <h4 style={styles.text} key={"h4-"+i}>{title}</h4>
+            <h6 style={{color:'#72a648', textAlign:'right'}}>Section {this.props.section}</h6>
+            <h5 style={styles.target} key={"h5-"+i}>{this.props.text}</h5>
+            <h6 style={{textAlign:"right", color:"#585a58"}} key={"h6-"+i}>{this.props.time}</h6>
+            <br></br>
+          </li>
+          </div>
+        </div>
+      <br></br>
+      <br></br>
+      </div>
+    )
+}});
 
 var AnnotationList = React.createClass({
+  getInitialState: function(){
+    return({
+
+      chapter:null
+    })
+  },
 
   listItems : function(){
 
@@ -31,11 +83,32 @@ var AnnotationList = React.createClass({
     var target = "";
 
     var section = String(this.props.section)
+
     var mapping = this.props.mapping
-    var filtered;
+
+    var filtered =[];
+    var filteredC = {};
+
+    if (this.props.chapter == null){
+      for (var key in mapping){
+        for (var key2 in mapping[key]){
+
+          filteredC[key2] = mapping[key][key2]
+        }
+      }
+    }
+    else if (mapping != null){
+      filteredC = mapping[this.props.chapter]
+    }
 
     if (this.props.section != null){
-      filtered = mapping[section]
+      filtered = filteredC[section]
+    }else if (this.props.chapter != null){
+      for (var sect in mapping[this.props.chapter]){
+        for (var anno in mapping[this.props.chapter][sect]){
+        filtered.push(mapping[this.props.chapter][sect][anno])
+      }
+      }
     }else{
       filtered = this.props.items
     };
@@ -48,36 +121,58 @@ var AnnotationList = React.createClass({
     var count = 0;
     for (i in filtered){
 
+      if (filtered[i].annotation == true){
+        styles.annotationBorder.border = '2px solid green'
+      }else{
+        styles.annotationBorder.border = '2px solid red'
+      }
+
       y ++ ;
 
       var uri = String(filtered[i].uri);
+      if (uri == 'http://localhost:3002/books/2'){
+        uri = 'http://localhost:3002/books/2/section/1'
+      }
+      if (uri.slice(-3).indexOf('/') > -1)
+        {
+          uri = uri + '  ';
+        }
 
-
-      if (filtered[i].target[0].selector.length >= 4){
-        target = JSON.stringify(filtered[i].target[0].selector[3].exact)
-
+      if (_.has(filtered[i].target[0], 'selector') == true){
+        if (filtered[i].target[0].selector.length >= 4){
+          target = JSON.stringify(filtered[i].target[0].selector[3].exact)
+        }
+      }else{
+        target = 'Page Note'
       };
 
+      if ((String(this.props.tag) == filtered[i].tags[0] || this.props.tag == null )
+    &&( (this.props.type == 'all')
+    || (this.props.type == 'highlights' && filtered[i].annotation == false)
+    || (this.props.type == 'annotations' && filtered[i].annotation == true))){
 
-      if (String(this.props.tag) == filtered[i].tags[0] || this.props.tag == null){
+      var title = JSON.stringify(filtered[i].text)
+
       count ++ ;
       allAnnotations.push(
-        <li key={"li-"+i}>
-          <h4 style={styles.text} key={"h4-"+i}>{y}. {JSON.stringify(filtered[i].text)}</h4>
-          <h5 style={styles.target} key={"h5-"+i}>{target}</h5>
-          <br></br>
-          <h6 style={{textAlign:"right", color:"#585a58"}} key={"h6-"+i}>{filtered[i].updated}</h6>
-          <br></br>
 
-        </li>
+        <div>
+          <AnnotationBox annotation={filtered[i].annotation} i={i} title={title}
+          section={uri.slice(-3)} text={target} time={filtered[i].updated}>
+          </AnnotationBox>
+        <br></br>
+        <br></br>
+        </div>
       );
 
     };
 
 
     };
-    if (count==0){
-      return(<h4>No Annotations have that tag</h4>)
+    if (count==0 && this.props.type != 'none'){
+      return(<h4>No Annotations or Highlights have that tag</h4>)
+    }else if(this.props.type == 'none'){
+      return(<h4>No Annotations or Highlights selected</h4>)
     }else{
       return (allAnnotations);
     }
@@ -103,7 +198,10 @@ var AnnotationList = React.createClass({
 
     )}else{
       return(
-      <h4>Loading...</h4>)
+      <div>
+      <ReactLoading type='spokes' color='#72a648'>
+      </ReactLoading>
+      </div>)
     }
   }
 });
--- summary-react/client/components/side.js
+++ summary-react/client/components/side.js
@@ -4,10 +4,21 @@ var axios = require('axios');
 var Annotation = require('./annotation.js');
 
 var Radium = require('radium');
-
+var Settings = require('./settings.js')
 import {Grid, Row, Col, Button} from 'react-bootstrap';
 
 
+var styles = {
+  button:{
+    outline:"none",
+    width:"83px"
+  },
+  allHighlights:{
+    outline:"none"
+  }
+};
+var annotationsButtonStyle = "primary"
+var highlightsButtonStyle = "primary"
 
 var Side = React.createClass({
   getInitialState:function(){
@@ -16,9 +27,18 @@ var Side = React.createClass({
       section:null,
       tag:null,
       clicked:null,
+      type:'all',
       Important:"default",
       Critical:"default",
       Question:"default"})
+  },
+  componentWillMount: function(){
+      var allTags = {};
+      for (var tag in Settings.tags){
+        allTags[Settings.tags[tag]] = "default"
+      };
+      this.setState(allTags)
+
   },
 
   changeSection: function(sect){
@@ -34,13 +54,30 @@ var Side = React.createClass({
   },
 
   getAllHighlights: function(){
-    this.setState({section:null})
-  },
+    this.setState({section:null, chapter:null})
 
+  },
+  toggleAnnotationsColor: function(){
+    if (annotationsButtonStyle == 'primary'){
+      annotationsButtonStyle = 'default'
+    }else{
+      annotationsButtonStyle = 'primary'
+    }
+  },
+  toggleHighlightsColor: function(){
+    if (highlightsButtonStyle == 'primary'){
+      highlightsButtonStyle = 'default'
+    }else{
+      highlightsButtonStyle = 'primary'
+    }
+  },
   toggleTag: function(newTag){
 
     var toggleOn = function(){
-      var allTags = ["Important", "Critical", "Question"];
+      var allTags = [];
+      for (var tag in Settings.tags){
+        allTags.push(Settings.tags[tag])
+      }
       var index = allTags.indexOf(newTag);
       allTags.splice(index, 1);
 
@@ -71,12 +108,35 @@ var Side = React.createClass({
     }
   },
 
-  sections:{
-    1:["1.1", "1.2", "1.3", "1.4"],
-    2:["2.1", "2.2", "2.3", "2.4"],
-    3:["3.1", "3.2", "3.3", "3.4"]
-  },
+  sections:Settings.sections,
+  toggleAnnotations: function(){
+    if (this.state.type == 'all'){
 
+
+      this.setState({type:'highlights'})
+    }else if (this.state.type == 'annotations'){
+      this.setState({type:'none'})
+    }else if (this.state.type == 'highlights'){
+      this.setState({type:'all'})
+    }else{
+      this.setState({type:'annotations'})
+    };
+    this.toggleAnnotationsColor()
+  },
+  toggleHighlights: function(){
+    if (this.state.type == 'all'){
+
+
+      this.setState({type:'annotations'})
+    }else if (this.state.type == 'annotations'){
+      this.setState({type:'all'})
+    }else if (this.state.type == 'highlights'){
+      this.setState({type:'none'})
+    }else{
+      this.setState({type:'highlights'})
+    };
+    this.toggleHighlightsColor()
+  },
   getSections: function(){
     var all = [];
     var chapterSections = [];
@@ -90,8 +150,11 @@ var Side = React.createClass({
 
         for(var j in this.sections[i]){
           var sectionButtonColor = "default"
+
           if (this.state.section != null){
-            if (this.sections[i][j] == this.state.section.slice(-3)){
+
+            if (this.sections[i][j] == this.state.section.split('/')[this.state.section.split('/').length - 1] ){
+
               sectionButtonColor = "primary"
             };
           };
@@ -99,7 +162,7 @@ var Side = React.createClass({
           chapterSections.push(
             <div >
               <li>
-                <Button key={"button"+{j}}bsStyle={sectionButtonColor} bsSize="xsmall" onClick={this.changeSection.bind(this, this.sections[i][j])}>Section {this.sections[i][j]}</Button>
+                <Button style={styles.button} key={"button"+{j}} bsStyle={sectionButtonColor} bsSize="xsmall" onClick={this.changeSection.bind(this, this.sections[i][j])}>Section {this.sections[i][j]}</Button>
               </li>
               <br></br>
             </div>
@@ -107,7 +170,7 @@ var Side = React.createClass({
         }
         all.push(
           <div >
-            <li ><Button bsStyle={chapterButtonColor} onClick={this.toggleChapter.bind(this, i)}>Chapter {i}</Button></li>
+            <li ><Button style={styles.button} bsStyle={chapterButtonColor} onClick={this.toggleChapter.bind(this, i)}>Chapter {i}</Button></li>
             <br></br>
             <ul style={{listStyle:"none"}}>{chapterSections}</ul>
           </div>
@@ -115,7 +178,7 @@ var Side = React.createClass({
       }else{
         all.push(
           <div>
-            <li><Button onClick={this.toggleChapter.bind(this, i)}>Chapter {i}</Button></li>
+            <li><Button style={styles.button} onClick={this.toggleChapter.bind(this, i)}>Chapter {i}</Button></li>
             <br></br>
           </div>
         )
@@ -129,17 +192,17 @@ var Side = React.createClass({
   render: function(){
     return(
 
-      <Row>
-        <Col md={2}>
-          <div><Button bsStyle="info" onClick={this.getAllHighlights.bind(this)}>All Highlights</Button></div>
+      <Row style={{textAlign:"center"}}>
+        <Col md={2} xs={12}>
+          <div><Button style={styles.allHighlights} bsStyle="info" onClick={this.getAllHighlights.bind(this)}>All Highlights</Button></div>
           <br></br>
           <div><ul style={{listStyle:"none"}}>{this.getSections()}</ul></div>
         </Col>
-        <Col md={6} mdOffset={1}>
-          <Annotation mapping={this.props.mapping} user={this.props.user} group = {this.props.group} items={this.props.items}
+        <Col md={6} mdOffset={1} xs={12}>
+          <Annotation type={this.state.type} chapter={this.state.chapter} mapping={this.props.mapping} user={this.props.user} group = {this.props.group} items={this.props.items}
           section={this.state.section} finished={this.props.finished} tag={this.state.tag}></Annotation>
         </Col>
-        <Col md={3}>
+        <Col md={3} xs={12}>
         <div>
           <div style={{paddingLeft:"17px"}} >
             <h3 style={{textAlign:"center"}}>Tags</h3>
@@ -147,18 +210,32 @@ var Side = React.createClass({
           </div>
           <Row>
             <Col md={4}>
-              <Button key="Important" onClick={this.toggleTag.bind(this, "Important")} bsSize="small" bsStyle={this.state["Important"]}>Important (!)</Button>
+              <Button style={styles.button} key={Settings.tags[0]} onClick={this.toggleTag.bind(this, Settings.tags[0])} bsSize="small" bsStyle={this.state["Important"]}>{Settings.tags[0]}</Button>
             </Col>
 
             <Col md={4}>
-              <div style={{paddingLeft:"7px"}}>
-              <Button key="Critical" onClick={this.toggleTag.bind(this, "Critical")} bsSize="small" bsStyle={this.state["Critical"]}>Critical (!!)</Button>
+              <div style={{paddingLeft:"0px"}}>
+              <Button style={styles.button} key={Settings.tags[1]} onClick={this.toggleTag.bind(this, Settings.tags[1])} bsSize="small" bsStyle={this.state["Critical"]}>{Settings.tags[1]}</Button>
               </div>
             </Col>
 
             <Col md={4}>
-              <Button key="Question" onClick={this.toggleTag.bind(this, "Question")} bsSize="small" bsStyle={this.state["Question"]}>Question (?)</Button>
+              <Button style={styles.button} key={Settings.tags[2]} onClick={this.toggleTag.bind(this, Settings.tags[2])} bsSize="small" bsStyle={this.state["Question"]}>{Settings.tags[2]}</Button>
             </Col>
+          </Row>
+          <br></br>
+          <div style={{paddingLeft:"17px"}} >
+            <h3 style={{textAlign:"center"}}>Type</h3>
+            <br></br>
+          </div>
+          <Row>
+            <Col md={6} style={{paddingLeft:"55px"}}>
+              <Button style={styles.button} key="Annotation" bsSize="small" bsStyle={annotationsButtonStyle} onClick={this.toggleAnnotations.bind(this)}>Annotations</Button>
+            </Col>
+            <Col md={6} style={{paddingRight:"45px"}}>
+              <Button style={styles.button} key="Highlights" bsSize="small" bsStyle={highlightsButtonStyle} onClick={this.toggleHighlights.bind(this)}>Highlights</Button>
+            </Col>
+
           </Row>
         </div>
         </Col>
